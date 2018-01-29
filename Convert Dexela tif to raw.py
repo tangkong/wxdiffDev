1ce1fd737b7564cd33752479832fc005c95730ac
tangkong <35379409+tangkong@users.noreply.github.com>
2018-01-29 16:24:22 +0000

    New script for updated tif

--- Convert Dexela tif to raw.py
+++ Convert Dexela tif to raw.py
@@ -3,7 +3,7 @@
 ### Python 2.7 (uses xrange)
 ### Date: 1/25/2018
 ### Author: Robert Tang-Kong
-### Version: 0.1
+### Version: 0.2
 ####################################################################################
 
 
@@ -13,38 +13,49 @@ from diffimagewindow import DiffImageWindow
 import os
 import sys
 from os.path import basename
-
-from PIL import Image
+import struct
 
 try:
+    # grab original path to reset later
     dexelaPath = os.getcwd()
+    # prompt for file to convert
     dlg = wx.FileDialog(
             MDIRoot, message="Choose a Dexela tif Image to convert",
-            style=wx.FD_OPEN | wx.FD_CHANGE_DIR
+            style=wx.FD_OPEN
             )
     dlg.ShowModal()
     dlg.Destroy()
     filename = dlg.GetPath()
 
-    # Import using Image pkg
-    imDx = Image.open(filename)
-    # Cast to array, dtype int16, save as raw
-    imDxarr = np.array(imDx.getdata())
+    # Open file
+    im = open(filename, 'rb')
+    im.seek(8) # remove first 8 bytes of header
+    arr = [] 
+    for i in range(int(23887872/2)): # Count knowing image size apriori
+        try:
+            buff = struct.unpack_from("<H",im.read(2))[0]
+            arr.append(buff)
+            # `<` means little endian; `H` means unsigned short aka int16 (2 bytes) 
+        except Exception as e:
+            print('error occured')
+            print(e)
+            break
 
+    nparr = np.array(arr)
+    print(nparr.shape)
+    print(nparr.dtype)
     dlgSave = wx.FileDialog(
             MDIRoot, message="Save as .raw",
-            style=wx.FD_SAVE | wx.FD_CHANGE_DIR
+            style=wx.FD_SAVE
             )
     dlgSave.ShowModal()
     dlgSave.Destroy()
     fileSavePath = dlgSave.GetPath()
 
-    imDxarr.astype('int16').tofile(fileSavePath)
+    nparr.astype('int16').tofile(fileSavePath)
 
     wx.MessageBox("Open new file as .raw/.bin file.  Data type is \"16 bit integer\", image size is 3072px x 3888px")
-    os.chdir(dexelaPath)
 
 except Exception as e:
     print('failure, resetting path')
-    os.chdir(dexelaPath)
     print(e)
